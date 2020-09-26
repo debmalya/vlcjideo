be9dea9fd6a0b3f1990b47634f026aa469628b59
deb malya <debmalya.jash@thalesgroup.com>
2020-09-26 12:59:32 +0800

    Integrating Spring boot and javafx

--- src/main/java/org/deb/vlcjideo/VlcjideoApplication.java
+++ src/main/java/org/deb/vlcjideo/VlcjideoApplication.java
@@ -1,13 +1,29 @@
 package org.deb.vlcjideo;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VlcjideoApplication {
+public class VlcjideoApplication extends Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VlcjideoApplication.class, args);
 	}
 
+	@Override
+	public void init() {
+		SpringApplication.run(getClass()).getAutowireCapableBeanFactory().autowireBean(this);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Pane helloFxPane = new Pane(new Label("Hello JavaFx"));
+		primaryStage.setScene(new Scene(helloFxPane));
+		primaryStage.show();
+	}
 }
