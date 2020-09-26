b23d3215c95d444d43c4db2d127065700a891b06
deb malya <debmalya.jash@thalesgroup.com>
2020-09-26 11:00:21 +0800

    Initial commit

---
+++ src/main/java/org/deb/vlcjideo/VlcjideoApplication.java
@@ -0,0 +1,13 @@
+package org.deb.vlcjideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlcjideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlcjideoApplication.class, args);
+	}
+
+}
